4d4c9923fe511b1600def0ba13a2298eab9381d7
Natalia Repkina <pn.88@mail.ru>
2024-07-06 12:30:02 +0300

    feat: add basic application structure

---
+++ youtube-app/src/app/detailed-card/detailed-card/detailed-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './detailed-card.component.html',
+  styleUrl: './detailed-card.component.scss'
+})
+export class DetailedCardComponent {
+
+}
---
+++ youtube-app/src/app/search/search-sorting/search-sorting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-sorting',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-sorting.component.html',
+  styleUrl: './search-sorting.component.scss'
+})
+export class SearchSortingComponent {
+
+}
