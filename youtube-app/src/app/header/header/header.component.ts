46f96330b7ea1f7019a98103ade1cf32f1b43810
Natalia Repkina <pn.88@mail.ru>
2024-07-07 12:00:34 +0300

    feat: add search form

--- youtube-app/src/app/header/header/header.component.ts
+++ youtube-app/src/app/header/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
+import { SearchFormComponent } from '../search-form/search-form.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, SearchFormComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
--- youtube-app/src/app/header/search-form/search-form.component.ts
+++ youtube-app/src/app/header/search-form/search-form.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatInputModule, MatButtonModule],
   templateUrl: './search-form.component.html',
-  styleUrl: './search-form.component.scss'
+  styleUrl: './search-form.component.scss',
 })
 export class SearchFormComponent {
+  inputControl = new FormControl('');
 
+  onSearch() {
+    console.log('Input Value:', this.inputControl.value);
+  }
 }
