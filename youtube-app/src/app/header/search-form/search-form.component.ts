51be8f7b130637078c0e194e26e3eb3b31ccd526
Natalia Repkina <pn.88@mail.ru>
2024-07-08 09:53:22 +0300

    feat: add button component

--- youtube-app/src/app/header/search-form/search-form.component.ts
+++ youtube-app/src/app/header/search-form/search-form.component.ts
@@ -3,12 +3,19 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ButtonComponent } from '../../button/button.component';
 import { SearchService } from '../../search/search.service';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatInputModule, MatButtonModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    ButtonComponent,
+  ],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.scss',
 })
