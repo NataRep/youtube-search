fca96fa5b5c879c92c6ce3cece6c28e1c6f83185
Natalia Repkina <pn.88@mail.ru>
2024-07-15 14:08:46 +0300

    init: create app with modules

--- youtube-app/src/app/app.component.ts
+++ youtube-app/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header/header.component';
-import { MainComponent } from './youtube/pages/main/main.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'youtube-app';
