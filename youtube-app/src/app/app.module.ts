09c7d3508c283c1dd8209afff476f32fefdee802
Natalia Repkina <pn.88@mail.ru>
2024-07-16 10:24:08 +0300

    feat: add angular material

--- youtube-app/src/app/app.module.ts
+++ youtube-app/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
