37988b1a270462655de3b054cd0b6e072df273fb
Natalia Repkina <pn.88@mail.ru>
2024-07-17 12:16:09 +0300

    feat: add search sort tools component

--- youtube-app/src/app/core/components/header/search-sort-tools/search-sort-tools.component.ts
+++ youtube-app/src/app/core/components/header/search-sort-tools/search-sort-tools.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CoreService } from '../../../services/core.service';
 
 @Component({
   selector: 'app-search-sort-tools',
   templateUrl: './search-sort-tools.component.html',
-  styleUrl: './search-sort-tools.component.scss'
+  styleUrl: './search-sort-tools.component.scss',
 })
 export class SearchSortToolsComponent {
+  constructor(private coreService: CoreService) {}
 
+  onSubmit(event: Event, term: string): void {
+    event.preventDefault();
+    this.coreService.filterTerm = term;
+  }
+
+  onSortDate(): void {
+    this.coreService.sortByDate = true;
+    this.coreService.sortByCountView = false;
+  }
+
+  onSortCount(): void {
+    this.coreService.sortByDate = false;
+    this.coreService.sortByCountView = true;
+  }
 }
--- youtube-app/src/app/core/services/core.service.ts
+++ youtube-app/src/app/core/services/core.service.ts
@@ -8,9 +8,14 @@ export class CoreService {
   private searchTermSubject = new BehaviorSubject<string>('');
   searchTerm$ = this.searchTermSubject.asObservable();
 
-  sortByDate: boolean = false;
-  sortByCountView: boolean = false;
-  filterTerm: string = '';
+  private filterTermSubject = new BehaviorSubject<string>('');
+  filterTerm$ = this.filterTermSubject.asObservable();
+
+  private sortByDateSubject = new BehaviorSubject<boolean>(false);
+  sortByDate$ = this.sortByDateSubject.asObservable();
+
+  private sortByCountViewSubject = new BehaviorSubject<boolean>(false);
+  sortByCountView$ = this.sortByCountViewSubject.asObservable();
 
   constructor() {}
 
@@ -20,5 +25,29 @@ export class CoreService {
 
   get searchTerm(): string {
     return this.searchTermSubject.value;
+  }
+
+  set filterTerm(value: string) {
+    this.filterTermSubject.next(value);
+  }
+
+  get filterTerm(): string {
+    return this.filterTermSubject.value;
+  }
+
+  set sortByDate(value: boolean) {
+    this.sortByDateSubject.next(value);
+  }
+
+  get sortByDate(): boolean {
+    return this.sortByDateSubject.value;
+  }
+
+  set sortByCountView(value: boolean) {
+    this.sortByCountViewSubject.next(value);
+  }
+
+  get sortByCountView(): boolean {
+    return this.sortByCountViewSubject.value;
   }
 }
