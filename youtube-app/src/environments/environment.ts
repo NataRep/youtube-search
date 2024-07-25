fb6f029b95ce8be39ab8dae558e76959dff0879a
Natalia Repkina <pn.88@mail.ru>
2024-07-25 14:48:44 +0300

    refactor: change environments

--- youtube-app/src/environments/environment.ts
+++ youtube-app/src/environments/environment.ts
@@ -1,11 +1,7 @@
 export const environment: Environment = {
-  apiUrl: `https://www.googleapis.com/youtube/v3`,
   apiKey: `AIzaSyBqCik0stmr7QP6W5eMCVmS9zyqBNPRu7A`,
-  apiKeyAlternative: 'AIzaSyBBHFaEB3gnBsfTDdjRsCb1uxEs87VRnyY',
 };
 
 interface Environment {
   apiKey: string;
-  apiUrl: string;
-  apiKeyAlternative: string;
 }
