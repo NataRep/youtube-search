d0f4e2f2a72214e01d6671ffa484ce06eec6a433
Natalia Repkina <pn.88@mail.ru>
2024-08-05 10:22:02 +0300

    init: install ngrx

--- youtube-app/src/app/app.module.ts
+++ youtube-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,10 +10,13 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { shorteningInterceptor } from './core/interceptors/shortening.interceptor';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, YoutubeModule],
+  imports: [BrowserModule, AppRoutingModule, YoutubeModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })],
   providers: [
     provideAnimationsAsync(),
     provideHttpClient(withFetch(), withInterceptors([shorteningInterceptor])),
