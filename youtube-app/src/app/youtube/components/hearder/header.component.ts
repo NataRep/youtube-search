3c9bb0d947364e5c81b09179fb695ab6349bbcbe
Natalia Repkina <pn.88@mail.ru>
2024-08-06 15:53:35 +0300

    feat: add icon for navigate to favorite page in the header

--- youtube-app/src/app/youtube/components/hearder/header.component.ts
+++ youtube-app/src/app/youtube/components/hearder/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,15 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   isSearchToolsVisible: boolean;
 
-  constructor() {
+  constructor(private router: Router) {
     this.isSearchToolsVisible = false;
   }
 
   showSearchTools() {
     this.isSearchToolsVisible = !this.isSearchToolsVisible;
+  }
+
+  goToFavorites() {
+    this.router.navigate(['/favorites']);
   }
 }
