b50e04332fadb51e133d4c9dbacce4b2e7fcf643
Natalia Repkina <pn.88@mail.ru>
2024-08-07 11:30:25 +0300

    refactor: separate auxiliary functions into a separate file

--- youtube-app/src/app/youtube/components/card-creation-form/card-creation-form.component.ts
+++ youtube-app/src/app/youtube/components/card-creation-form/card-creation-form.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormArray,
-  FormBuilder,
-  FormGroup,
-  ValidationErrors,
-  ValidatorFn,
-  Validators,
-} from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Item } from '../../../core/models/search-item.model';
+import {
+  generateRandomId,
+  imageUrlValidator,
+  pastDateValidator,
+  videoUrlValidator,
+} from './helper';
 
 @Component({
   selector: 'app-card-creation-form',
@@ -122,55 +120,4 @@ export class CardCreationFormComponent implements OnInit {
       this.tags.push(this.createTag());
     }
   }
-}
-
-export function pastDateValidator(): ValidatorFn {
-  return (
-    control: AbstractControl
-  ): { [key: string]: ValidationErrors } | null => {
-    const dateValue = control.value;
-    const error: ValidationErrors = {
-      pastDate: 'The date must be in the past',
-    };
-    if (dateValue && new Date(dateValue) > new Date()) {
-      return error;
-    }
-    return null;
-  };
-}
-
-// валидация ссылки на изображение
-export function imageUrlValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const url = control.value;
-    const regex = /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|bmp|webp|svg))$/i;
-    const valid = regex.test(url);
-    return valid
-      ? null
-      : { invalidImageUrl: 'The URL is not a valid image link' };
-  };
-}
-
-// валидация ссылки на видео
-export function videoUrlValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const url = control.value;
-    const regex = /^(https?:\/\/.*\.(?:mp4|mov|avi|wmv|flv|mkv|webm))$/i;
-    const valid = regex.test(url);
-    return valid
-      ? null
-      : { invalidVideoUrl: 'The URL is not a valid video link' };
-  };
-}
-
-//генерируем рандомный id длинной 11 символов
-function generateRandomId(length: number = 11): string {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
 }
---
+++ youtube-app/src/app/youtube/components/card-creation-form/helper.ts
@@ -0,0 +1,53 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+// валидация даты
+export function pastDateValidator(): ValidatorFn {
+  return (
+    control: AbstractControl
+  ): { [key: string]: ValidationErrors } | null => {
+    const dateValue = control.value;
+    const error: ValidationErrors = {
+      pastDate: 'The date must be in the past',
+    };
+    if (dateValue && new Date(dateValue) > new Date()) {
+      return error;
+    }
+    return null;
+  };
+}
+
+// валидация ссылки на изображение
+export function imageUrlValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const url = control.value;
+    const regex = /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|bmp|webp|svg))$/i;
+    const valid = regex.test(url);
+    return valid
+      ? null
+      : { invalidImageUrl: 'The URL is not a valid image link' };
+  };
+}
+
+// валидация ссылки на видео
+export function videoUrlValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const url = control.value;
+    const regex = /^(https?:\/\/.*\.(?:mp4|mov|avi|wmv|flv|mkv|webm))$/i;
+    const valid = regex.test(url);
+    return valid
+      ? null
+      : { invalidVideoUrl: 'The URL is not a valid video link' };
+  };
+}
+
+//генерируем рандомный id длинной 11 символов
+export function generateRandomId(length: number = 11): string {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
